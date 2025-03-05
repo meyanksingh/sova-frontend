6d6e66a8e604cb83e9a439b04290b13626ff2780
Meyank Singh <meyank24@gmail.com>
2025-03-05 06:19:58 +0530

    Update api.ts

--- lib/api.ts
+++ lib/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://20.244.9.82:8080/api"
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "https://api.meyank.me/api"
 
 const api = axios.create({
   baseURL: API_BASE_URL,
